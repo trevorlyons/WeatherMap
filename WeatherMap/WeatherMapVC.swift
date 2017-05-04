abf9e5028dd31d181a2a86efe226d6f797522770
trevorlyons <trevorjclyons@hotmail.com>
2017-05-04 12:52:08 +0200

    Completed basic UI

--- WeatherMap/WeatherMapVC.swift
+++ WeatherMap/WeatherMapVC.swift
@@ -7,17 +7,17 @@
 //
 
 import UIKit
+import MapKit
 
-class WeatherMapVC: UIViewController {
+class WeatherMapVC: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        mapView.delegate = self
+        
     }
 
 
