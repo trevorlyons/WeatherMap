000ca5a419e8f296b82d0adbe2cfe8dcdb85e95b
trevorlyons <trevorjclyons@hotmail.com>
2017-05-05 12:08:08 +0200

    Attempted popup for daily weather and settings. Need to change to modal presentation

---
+++ WeatherMap/SettingsVC.swift
@@ -0,0 +1,29 @@
+//
+//  SettingsVC.swift
+//  WeatherMap
+//
+//  Created by Trevor Lyons on 2017-05-04.
+//  Copyright © 2017 Trevor Lyons. All rights reserved.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.view.superview?.layer.cornerRadius = 0.0
+    }
+
+    @IBAction func backButtonPressed(_ sender: Any) {
+        dismiss(animated: false, completion: nil)
+    }
+    
+}
--- WeatherMap/WeatherMapVC.swift
+++ WeatherMap/WeatherMapVC.swift
@@ -9,9 +9,10 @@
 import UIKit
 import MapKit
 
-class WeatherMapVC: UIViewController, MKMapViewDelegate {
+class WeatherMapVC: UIViewController, MKMapViewDelegate, UIPopoverPresentationControllerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +20,38 @@ class WeatherMapVC: UIViewController, MKMapViewDelegate {
         mapView.delegate = self
         
     }
+    
+    
+    override func prepare(for segue:UIStoryboardSegue, sender:Any!) {
+        if segue.identifier == "settings" {
+            let settingsController = segue.destination as! SettingsVC
+            let screenSize = UIScreen.main.bounds
+            let screenWidth = screenSize.width
+            let screenHeight = screenSize.height
+            settingsController.preferredContentSize = CGSize(width: screenWidth, height: screenHeight*0.75)
+            
+            let popoverController = settingsController.popoverPresentationController
+            
+            if popoverController != nil {
+                popoverController!.delegate = self
+                popoverController!.backgroundColor = UIColor.white
+                popoverController!.permittedArrowDirections = UIPopoverArrowDirection(rawValue: 0)
 
+            }
+        }
+    }
+    
+    func adaptivePresentationStyle(for controller: UIPresentationController, traitCollection: UITraitCollection) -> UIModalPresentationStyle {
+        return .none
+    }
 
+    @IBAction func settingsBtnPressed(_ sender: Any) {
+    }
+    
+
+    @IBAction func searchBtnPressed(_ sender: Any) {
+    }
+    
+    
 }
 
