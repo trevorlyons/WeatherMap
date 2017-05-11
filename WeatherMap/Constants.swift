a595749d73b7dfec5ebea6c1e8b1a9d95ac7a083
trevorlyons <trevorjclyons@hotmail.com>
2017-05-11 21:55:09 +0200

    Auto annotating cities on map implemented

--- WeatherMap/Constants.swift
+++ WeatherMap/Constants.swift
@@ -8,17 +8,9 @@
 
 import Foundation
 
-// DarkSky API
-
-let DarkSkyURL = "https://api.darksky.net/forecast/"
-let DarkSkyAPI_KEY = "be92ba28eac77e982de31df6fec9515e/"
-let latitude = "42.8821,"
-let longitude = "-8.541"
-
 typealias DownloadComplete = () -> ()
 
-//let CURRENT_WEATHER_URL = "\(DarkSkyURL)\(DarkSkyAPI_KEY)\(latitude)\(longitude)"
 
 let CURRENT_WEATHER_URL = "https://api.darksky.net/forecast/be92ba28eac77e982de31df6fec9515e/42.8821,-8.541?units=si"
 
-//let CURRENT_WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?lat=42.8821&lon=-8.5419&appid=d9edbc6106170dc5ca87733c4b46128d"
+var MAP_WEATHER_URL = "http://api.openweathermap.org/data/2.5/box/city?bbox=\(Location.sharedInstance.lowerLeftLongitude!),\(Location.sharedInstance.lowerLeftLatitude!),\(Location.sharedInstance.upperRightLongitude!),\(Location.sharedInstance.upperRightLatitude!),10&appid=d9edbc6106170dc5ca87733c4b46128d"
---
+++ WeatherMap/MapAnnotation.swift
@@ -0,0 +1,66 @@
+//
+//  MapAnnotations.swift
+//  WeatherMap
+//
+//  Created by Trevor Lyons on 2017-05-11.
+//  Copyright © 2017 Trevor Lyons. All rights reserved.
+//
+
+import Foundation
+
+class MapAnnotation {
+    
+    private var _latitude: Double!
+    private var _longitude: Double!
+    private var _cityName: String!
+    private var _temperature: Double!
+    
+    var latitude: Double {
+        if _latitude == nil {
+            _latitude = 0.0
+        }
+        return _latitude
+    }
+    
+    var longitude: Double {
+        if _longitude == nil {
+            _longitude = 0.0
+        }
+        return _longitude
+    }
+    
+    var cityName: String {
+        if _cityName == nil {
+            _cityName = ""
+        }
+        return _cityName
+    }
+    
+    var temperature: Double {
+        if _temperature == nil {
+            _temperature = 0.0
+        }
+        return _temperature
+    }
+    
+    init(locationDict: Dictionary<String, AnyObject>) {
+        if let coord = locationDict["coord"] as? Dictionary<String, AnyObject> {
+            if let Lat = coord["Lat"] as? Double {
+                self._latitude = Lat
+            }
+            if let Lon = coord["Lon"] as? Double {
+                self._longitude = Lon
+            }
+        }
+        if let name = locationDict["name"] as? String {
+            self._cityName = name
+        }
+        if let main = locationDict["main"] as? Dictionary<String, AnyObject> {
+            if let temp = main["temp"] as? Double {
+                self._temperature = temp
+            }
+        }
+    }
+    
+    
+}
