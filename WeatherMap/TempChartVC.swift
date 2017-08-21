46d64aad64b4e6d9845a657f68919abe96934d2b
trevorlyons <trevorjclyons@hotmail.com>
2017-08-21 11:52:36 +0200

    additions before adding Charts

---
+++ WeatherMap/TempChartVC.swift
@@ -0,0 +1,20 @@
+//
+//  TempChartVC.swift
+//  WeatherMap
+//
+//  Created by Trevor Lyons on 2017-08-20.
+//  Copyright © 2017 Trevor Lyons. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class TempChartVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+}
