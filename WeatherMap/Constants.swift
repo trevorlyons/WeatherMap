b7425a4634449a82152eabc7d0264ca8a1ded517
Trevor Lyons <trevorjclyons@hotmail.com>
2018-02-20 16:11:18 +0100

    Update Constants.swift

--- WeatherMap/Constants.swift
+++ WeatherMap/Constants.swift
@@ -12,10 +12,10 @@ typealias DownloadComplete = () -> ()
 
 typealias JSONDictionary = [String:Any]
 
-let darkSkyUrl = "https://api.darksky.net/forecast/be92ba28eac77e982de31df6fec9515e/"
+let darkSkyUrl = ""
 let seperator = ","
 
-let CURRENT_WEATHER_URL = "https://api.darksky.net/forecast/be92ba28eac77e982de31df6fec9515e/42.8821,-8.541?units=si"
+let CURRENT_WEATHER_URL = ""
 
 
 
